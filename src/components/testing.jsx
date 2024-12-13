6e48da9915378dd739e89fd155b0f7abfac4577d
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-13 13:32:04 +0530

    add design css

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -15,10 +15,11 @@ export default function Testing(){
     }
 
     return(
-        <div>
-            <button onClick={decrement}>-</button>
-            <span>{count}</span>
-            <button onClick={increment}>+</button>
+        <div className="h-64 gap-3 w-64 flex justify-center items-center border border-black">
+            <button className="w-20 text-4xl h-20 rounded-full bg-red-400 cursor-pointer" onClick={decrement}>-</button>
+            <span className="text-4xl">{count}</span>
+
+            <button className="w-20 h-20 text-4xl rounded-full bg-red-400 cursor-pointer" onClick={increment}>+</button>
         </div>
     )
 
