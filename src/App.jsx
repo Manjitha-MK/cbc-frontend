7463d21000fc80da65c2ea243102b5c0dd8271ca
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-19 15:07:15 +0530

    create adminHomePagejsx file and connetc Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes ,Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
+import SignUpPage from './pages/signUpPage'
+import AdminHomePage from './pages/adminHomePage'
+
 
 
 
@@ -14,6 +17,8 @@ function App() {
       <Routes path="/*">
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/signup" element={<SignUpPage/>}></Route>
+        <Route path="/admin/*" element={<AdminHomePage/>}></Route>
         <Route path='/*' element={<HomePage/>}></Route>
       </Routes>
       </BrowserRouter>
---
+++ src/pages/adminHomePage.jsx
@@ -0,0 +1,7 @@
+export default function AdminHomePage(){
+    return(
+        <div>
+            <h1>Admin Home page</h1>
+        </div>
+    )
+}
