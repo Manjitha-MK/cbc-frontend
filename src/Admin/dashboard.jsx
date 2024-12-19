f62d70bc7b8382284fd02a1105a799283046e32d
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-20 00:51:40 +0530

    install axios and create order,products,dashboard jsx files in admin folder

---
+++ src/Admin/dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return(
+        <div id="dash">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,7 +1,24 @@
-export default function AdminHomePage(){
-    return(
-        <div>
-            <h1>Admin Home page</h1>
-        </div>
-    )
+import { Link, Routes, Route } from "react-router-dom";
+import Dashboard from "../Admin/dashboard";
+import Products from "../Admin/products";
+
+export default function AdminHomePage() {
+  return (
+    <div className="flex">
+      <div className="w-[20%] h-screen items-center flex flex-col bg-blue-400">
+        <Link to="/admin/dashboard" >Dashboard</Link>
+        <Link to="/admin/orders">Orders</Link>
+        <Link to="/admin/products">Products</Link>
+      </div>
+      <div className="w-[80%] bg-red-500 h-screen flex ">
+        
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/orders" element={<h1>Orders</h1>}/>
+            <Route path="/products" element={<Products/>}/>
+        </Routes>
+
+      </div>
+    </div>
+  );
 }
