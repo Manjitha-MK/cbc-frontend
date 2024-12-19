f05cdccd3a431498ff1def4c0eaf46ee47133c08
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-19 23:36:33 +0530

    All expirements are deleted🫡

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,12 +1,7 @@
-import UserData from "../components/userData"
-import Testing from "../components/testing"
-
 export default function AdminHomePage(){
     return(
         <div>
             <h1>Admin Home page</h1>
-            <UserData/>
-            <Testing/>
         </div>
     )
 }
