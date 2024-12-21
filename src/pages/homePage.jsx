73de4034a4e6c8978e3b87ba07ecc47f906ce255
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-21 12:06:35 +0530

    remove footer section

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -58,15 +58,7 @@ const HomePage = () => {
           </div>
         </div>
       </section>
-
-      {/* Footer Section */}
-      <footer className="py-6 bg-gray-800">
-        <div className="container mx-auto text-center">
-          <p className="text-gray-500">
-            &copy; 2024 My Website. All Rights Reserved.
-          </p>
-        </div>
-      </footer>
+     
     </div>
   );
 };
