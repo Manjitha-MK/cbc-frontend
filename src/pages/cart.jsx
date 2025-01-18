28cc8ebf6704f63f3419ce4c70b359bd5ad8b393
Manjitha-MK <manjithataridu123@gmail.com>
2025-01-18 13:23:05 +0530

    create cart jsx file and Cart function

---
+++ src/pages/cart.jsx
@@ -0,0 +1,7 @@
+export default function Cart(){
+    return(
+        <div className="flex flex-wrap justify-center w-full h-full overflow-y-scroll">
+            <h1>cart</h1>
+        </div>
+    )
+}
