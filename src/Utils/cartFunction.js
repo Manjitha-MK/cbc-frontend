f7f199c051c2b391e436ce61c36a112f3f2f7411
Manjitha-MK <manjithataridu123@gmail.com>
2025-01-20 15:50:07 +0530

    create deleteItem function

--- src/Utils/cartFunction.js
+++ src/Utils/cartFunction.js
@@ -43,3 +43,14 @@ export function saveCart(cart) {
 export function clearCart() {
   localStorage.removeItem("cart");
 }
+
+export function deleteItem(productId){
+  const cart = loadCart()
+  const index = cart.findIndex((item) => {
+    return item.productId == productId;
+  });
+
+  if(index!=-1){
+    cart.splice(index,1)
+  }
+}
