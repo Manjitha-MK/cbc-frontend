3dfce46686bed04b6dff62d63764edbf38af684a
Manjitha-MK <manjithataridu123@gmail.com>
2025-06-27 13:22:45 +0530

    Add handleChange function to update formData dynamically

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -13,6 +13,10 @@ const Contact = () => {
     message: "",
   });
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
 
   return (
     <div className="min-h-screen px-4 py-8 sm:px-6 lg:px-16">
